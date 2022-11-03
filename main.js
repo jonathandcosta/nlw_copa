f7095cd61f101c20da97558b09e93e9ec41abec7
Jonathan Costa <jonathanduartecosta@gmail.com>
2022-11-02 21:59:02 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+function creatGame(player1, hour, player2) {
+  return `
+  <li>
+  <img src="./assets/icons_flags/icon=${player1}.svg" alt="banderia do ${player1}">
+  <strong>${hour}</strong>
+  <img src="./assets/icons_flags/icon=${player2}.svg" alt="bandeira de ${player2}">
+</li>`;
+}
+
+function creatCard(date, day, games) {
+  return `
+  <div class="card">
+        <h2>${date}<span>${day}</span></h2>
+        <ul>
+         ${games}
+        </ul>
+      </div>
+  `;
+}
+
+document.querySelector("#app").innerHTML = `
+<header>
+<img src="./assets/img/logo_desktop.svg" alt="logo">
+</header>
+<main id="cards">
+      ${creatCard("24/11", "quinta", creatGame("brazil", "16:00", "serbia"))}
+      ${creatCard(
+        "28/11",
+        "segunda",
+        creatGame("switzerland", "13:00", "brazil") +
+          creatGame("portugal", "15:00", "uruguay")
+      )}
+      ${creatCard("02/12", "sexta", creatGame("brazil", "16:00", "cameroon"))}
+</main>
+`;
