95b5633449b9fcccff2cdd9a876c50fe7c85029d
Jonathan Costa <jonathanduartecosta@gmail.com>
2022-11-06 20:08:45 -0300

    adicionado um titulo e o README

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align="center"> NLW Explorer - Copa 2022 </h1>
+
+<p align="center">
+Evento exclusivo e gratuito, promovido pela Rocketseat para ensino de tecnologias WEB.
+</p>
+
+<p align="center">
+  <img alt="copa 2022" src="./github/preview.jpg">
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript e JSON
+- Git e GitHub
+
+<br>
+
+## 💻 Projeto
+
+O projeto NLW Copa apresneta o calendário da copa 2022
+
+<br>
--- main.js
+++ main.js
@@ -19,25 +19,7 @@ function creatCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  creatCard(
-    "24/11",
-    "quinta",
-    creatGame("brazil", "16:00", "serbia") +
-      creatGame("brazil", "16:00", "serbia") +
-      creatGame("brazil", "16:00", "serbia") +
-      creatGame("brazil", "16:00", "serbia")
-  ) +
-  creatCard(
-    "28/11",
-    "segunda",
-    creatGame("switzerland", "13:00", "brazil") +
-      creatGame("portugal", "15:00", "uruguay") +
-      creatGame("portugal", "15:00", "uruguay")
-  ) +
-  creatCard(
-    "02/12",
-    "sexta",
-    creatGame("brazil", "16:00", "cameroon") +
-      creatGame("brazil", "16:00", "serbia") +
-      creatGame("brazil", "16:00", "serbia")
-  );
+  creatCard("24/11", "quinta", creatGame("brazil", "16:00", "serbia")) +
+  creatCard("28/11", "segunda", creatGame("brazil", "13:00", "switzerland")) +
+  creatCard("02/12", "sexta", creatGame("cameroon", "16:00", "brazil"));
+/* ----------- FASE DE GRUPO ------------ */
